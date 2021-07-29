425c8d2b0954d1153f5d3e087370a4e8eb762d82
epicalex4444 <alexroe@email.com>
2021-07-30 05:30:55 +1000

    move tidying

---
+++ dataCollectionMod/Main.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Assets.Scripts.Utils;
+using Assets.Scripts.Unity.UI_New.InGame;
+
+namespace paragonLog {
+
+    public class Main : MelonMod {
+        public override void OnUpdate() {
+            base.OnUpdate();
+            
+            if (Input.GetKeyDown(KeyCode.Alpha8)) {
+                foreach (var tower in InGame.instance.bridge.GetAllTowers()) {
+                    if (tower.Def.isParagon) {
+                        FileIOUtil.LogToFile("../../../../Documents/btd6/paragons/DartParagon" + tower.GetParagonDegreeMutator().degree + ".json", tower.Def);
+                    }
+                }
+            }
+        }
+    }
+}
